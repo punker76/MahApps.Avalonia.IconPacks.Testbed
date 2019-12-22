daf3e8af7a1f4942340df3840fb91f9c51884d65
punker76 <punkerat76@gmail.com>
2019-12-22 23:04:24 +0100

    Derive DataToGeometryConverter from MarkupConverter

--- src/AvaloniaIconPacks/DataToGeometryConverter.cs
+++ src/AvaloniaIconPacks/DataToGeometryConverter.cs
@@ -1,27 +1,16 @@
 using System;
 using System.Globalization;
-using Avalonia.Data.Converters;
 using Avalonia.Media;
 
-namespace MahApps.Metro.IconPacks
+namespace MahApps.Metro.IconPacks.Converter
 {
     /// <summary>
     /// GeometryConverter - Converter class for converting instances of other types to and from Geometry instances
     /// </summary>
-    public sealed class DataToGeometryConverter : IValueConverter
+    public sealed class DataToGeometryConverter : MarkupConverter
     {
-        /// <summary>Attempts to convert to a Geometry from the given object.</summary>
-        /// <param name="value">The object to convert to an instance of Geometry.</param>
-        /// <param name="targetType">The type of the target.</param>
-        /// <param name="parameter">A user-defined parameter.</param>
-        /// <param name="culture">The culture to use.</param>
-        /// <returns>The Geometry which was constructed.</returns>
-        /// <remarks>
-        /// This method should not throw exceptions. If the value is not convertible, return
-        /// a <see cref="T:Avalonia.Data.BindingNotification" /> in an error state. Any exceptions thrown will be
-        /// treated as an application exception.
-        /// </remarks>
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        /// <inheritdoc />
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is string data)
             {
@@ -31,18 +20,8 @@ namespace MahApps.Metro.IconPacks
             return null;
         }
 
-        /// <summary>Converts a value.</summary>
-        /// <param name="value">The value to convert.</param>
-        /// <param name="targetType">The type of the target.</param>
-        /// <param name="parameter">A user-defined parameter.</param>
-        /// <param name="culture">The culture to use.</param>
-        /// <returns>The converted value.</returns>
-        /// <remarks>
-        /// This method should not throw exceptions. If the value is not convertible, return
-        /// a <see cref="T:Avalonia.Data.BindingNotification" /> in an error state. Any exceptions thrown will be
-        /// treated as an application exception.
-        /// </remarks>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        /// <inheritdoc />
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
